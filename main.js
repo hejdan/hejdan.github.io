ccd4d7bce530e8ea744cbf6a70bd3e1eb6f12b4f
hejdan <36206557+hejdan@users.noreply.github.com>
2019-07-20 00:56:34 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ let Youtube = document.querySelector("#Youtube");
 let Netflix = document.querySelector("#Netflix");
 let Ae27ff = document.querySelector("#Ae27ff");
 
+let clock = document.querySelector("#clock");
+
+
 Reddit.addEventListener("click", () => {
     self.location.href = "https://www.reddit.com";
 });
@@ -23,3 +26,23 @@ Netflix.addEventListener("click", () => {
 Ae27ff.addEventListener("click", () => {
     self.location.href = "http://ae27ff.meme.tips";
 });
+
+
+function updateTime() {
+    var currentTime = new Date()
+    var hours = currentTime.getHours()
+    var minutes = currentTime.getMinutes()
+    var seconds = currentTime.getSeconds()
+    if (hours < 10) {
+        hours = "0" + hours
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds
+    }
+    var t_str = hours + ":" + minutes + ":" + seconds;
+    clock.innerHTML = t_str;
+}
+setInterval(updateTime, 1000);
