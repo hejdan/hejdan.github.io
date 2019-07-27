4836265639ccdb3142c0967b7f006a4b40098205
hejdan <36206557+hejdan@users.noreply.github.com>
2019-07-27 07:55:44 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,15 @@ let Facebook = document.querySelector("#Facebook");
 let Youtube = document.querySelector("#Youtube");
 let Netflix = document.querySelector("#Netflix");
 let Ae27ff = document.querySelector("#Ae27ff");
+let Github = document.querySelector("#Github");
 
 let clock = document.querySelector("#clock");
+
+let Joke = document.querySelector(".Joke");
+
+const api = 'https://official-joke-api.appspot.com/jokes/programming/random';
+
+
 
 
 Reddit.addEventListener("click", () => {
@@ -27,6 +34,10 @@ Ae27ff.addEventListener("click", () => {
     self.location.href = "http://ae27ff.meme.tips";
 });
 
+Github.addEventListener("click", () => {
+    self.location.href = "https://www.github.com";
+});
+
 
 function updateTime() {
     var currentTime = new Date()
@@ -44,5 +55,27 @@ function updateTime() {
     }
     var t_str = hours + ":" + minutes + ":" + seconds;
     clock.innerHTML = t_str;
+
 }
 setInterval(updateTime, 1);
+
+function GenerateJoke() {
+    fetch(api)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            Joke.innerHTML = data[0].setup;
+
+            Joke.addEventListener("click", () => {
+
+                if (Joke.innerHTML == data[0].setup) {
+                    Joke.innerHTML = data[0].punchline;
+                } else if (Joke.innerHTML == data[0].punchline) {
+                    Joke.innerHTML = data[0].setup;
+                }
+
+            });
+
+        })
+}
