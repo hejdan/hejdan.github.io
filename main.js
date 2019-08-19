da563b83bb2eb20a5c822801b761c6e92dc64caf
hejdan <36206557+hejdan@users.noreply.github.com>
2019-08-19 09:02:26 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,36 @@
+//Access the html divs ich
 let Youtube = document.querySelector("#youtube");
 let Reddit = document.querySelector("#reddit");
 let Netflix = document.querySelector("#netflix");
 let Github = document.querySelector("#github");
 let Facebook = document.querySelector("#facebook");
 let Ae = document.querySelector("#ae");
-let Body = document.querySelector("#body");
+let Joke = document.querySelector(".Joke");
+
+//API
+const api = "https://official-joke-api.appspot.com/jokes/programming/random";
+
+function GenerateJoke() {
+  fetch(api)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      Joke.innerHTML = data[0].setup;
+
+      Joke.addEventListener("click", () => {
+        if (Joke.innerHTML == data[0].setup) {
+          Joke.innerHTML = data[0].punchline;
+        } else if (Joke.innerHTML == data[0].punchline) {
+          Joke.innerHTML = data[0].setup;
+        }
+      });
+    });
+}
 
 //Goes to the hyperlink connected to the image if clicked
-
 Youtube.addEventListener("click", () => {
-  self.location.href = "https://www.youtube.com";
+  Youtube.style.height = "100vh";
 });
 
 Reddit.addEventListener("click", () => {
@@ -30,55 +51,4 @@ Facebook.addEventListener("click", () => {
 
 Ae.addEventListener("click", () => {
   self.location.href = "http://ae27ff.meme.tips";
-});
-
-//Changes the bodys background color depending of which image mouse is over
-
-Youtube.addEventListener("mouseenter", () => {
-  body.style.background = "#911f00";
-});
-
-Reddit.addEventListener("mouseenter", () => {
-  body.style.background = "#cf6400";
-});
-
-Netflix.addEventListener("mouseenter", () => {
-  body.style.background = "#0f0f0f";
-});
-
-Github.addEventListener("mouseenter", () => {
-  body.style.background = "#2b2b2b";
-});
-
-Facebook.addEventListener("mouseenter", () => {
-  body.style.background = "#335bff";
-});
-
-Ae.addEventListener("mouseenter", () => {
-  body.style.background = "green";
-});
-
-//removing bodys background color
-Youtube.addEventListener("mouseout", () => {
-  body.style.background = "#484848";
-});
-
-Reddit.addEventListener("mouseout", () => {
-  body.style.background = "#484848";
-});
-
-Netflix.addEventListener("mouseout", () => {
-  body.style.background = "#484848";
-});
-
-Github.addEventListener("mouseout", () => {
-  body.style.background = "#484848";
-});
-
-Facebook.addEventListener("mouseout", () => {
-  body.style.background = "#484848";
-});
-
-Ae.addEventListener("mouseout", () => {
-  body.style.background = "#484848";
 });
